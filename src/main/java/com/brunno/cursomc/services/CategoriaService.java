d20e9891d6489aa994125f95d3941fbace433cb9
Brunno Ribeiro <brunnorb@gmail.com>
2018-09-04 15:55:02 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/brunno/cursomc/services/CategoriaService.java
+++ src/main/java/com/brunno/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.brunno.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,10 +41,14 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
-		} catch(DataIntegrityViolationException e) {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto!");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
