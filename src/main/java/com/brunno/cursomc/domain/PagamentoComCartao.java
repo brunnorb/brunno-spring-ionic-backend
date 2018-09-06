62f1dd4fc8f62254155b95bfd3897ed9dee85b65
Brunno Ribeiro <brunnorb@gmail.com>
2018-09-06 15:08:44 -0300

    Inserindo Pedido

--- src/main/java/com/brunno/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/brunno/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.brunno.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.brunno.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
