319b23e8684999e30bcb577639a66a4bd18c77ae
Brunno Ribeiro <brunnorb@gmail.com>
2018-09-06 15:34:18 -0300

    Criando profile de test

--- src/main/java/com/brunno/cursomc/config/JacksonConfig.java
+++ src/main/java/com/brunno/cursomc/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package com.brunno.cursomc.domain.config;
+package com.brunno.cursomc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
