06765024f22a2c6e6ab7ac903a8f1a4a87353eea
Brunno Ribeiro <brunnorb@gmail.com>
2018-09-19 13:09:54 -0300

    Correcao ProcFile

--- src/main/java/com/brunno/cursomc/CursomcApplication.java
+++ src/main/java/com/brunno/cursomc/CursomcApplication.java
@@ -1,39 +1,12 @@
 package com.brunno.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.brunno.cursomc.domain.Categoria;
-import com.brunno.cursomc.domain.Cidade;
-import com.brunno.cursomc.domain.Cliente;
-import com.brunno.cursomc.domain.Endereco;
-import com.brunno.cursomc.domain.Estado;
-import com.brunno.cursomc.domain.ItemPedido;
-import com.brunno.cursomc.domain.Pagamento;
-import com.brunno.cursomc.domain.PagamentoComBoleto;
-import com.brunno.cursomc.domain.PagamentoComCartao;
-import com.brunno.cursomc.domain.Pedido;
-import com.brunno.cursomc.domain.Produto;
-import com.brunno.cursomc.domain.enums.EstadoPagamento;
-import com.brunno.cursomc.domain.enums.TipoCliente;
-import com.brunno.cursomc.repositories.CategoriaRepository;
-import com.brunno.cursomc.repositories.CidadeRepository;
-import com.brunno.cursomc.repositories.ClienteRepository;
-import com.brunno.cursomc.repositories.EnderecoRepository;
-import com.brunno.cursomc.repositories.EstadoRepository;
-import com.brunno.cursomc.repositories.ItemPedidoRepository;
-import com.brunno.cursomc.repositories.PagamentoRepository;
-import com.brunno.cursomc.repositories.PedidoRepository;
-import com.brunno.cursomc.repositories.ProdutoRepository;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
+ 
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
