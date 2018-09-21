cb374bfe2e8573a6e96bcdb34d8cec6a05a3dc10
Brunno Ribeiro <brunnorb@gmail.com>
2018-09-21 12:46:05 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/brunno/cursomc/services/ClienteService.java
+++ src/main/java/com/brunno/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.brunno.cursomc.domain.Cidade;
 import com.brunno.cursomc.domain.Cliente;
 import com.brunno.cursomc.domain.Endereco;
+import com.brunno.cursomc.domain.enums.Perfil;
 import com.brunno.cursomc.domain.enums.TipoCliente;
 import com.brunno.cursomc.dto.ClienteDTO;
 import com.brunno.cursomc.dto.ClienteNewDTO;
 import com.brunno.cursomc.repositories.ClienteRepository;
 import com.brunno.cursomc.repositories.EnderecoRepository;
+import com.brunno.cursomc.security.UserSS;
+import com.brunno.cursomc.services.exceptions.AuthorizationException;
 import com.brunno.cursomc.services.exceptions.DataIntegrityException;
 import com.brunno.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,13 @@ public class ClienteService {
 	private BCryptPasswordEncoder pe;
 
 	public Cliente find(Integer id) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+			
+		}
+		
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
